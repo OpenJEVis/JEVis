9258a228afb64e388d24bb578ab90eee8aa9a9e0
Florian Simon <florian.simon@envidatec.com>
2018-04-19 12:48:45 +0200

    Copy butfix and additonal output

--- install.sh
+++ install.sh
@@ -9,11 +9,14 @@ if [ -z "${JEVIS_HOME}" ]; then
 fi
 
 # Check if all programm are installed
-checkprogram ()
+checkprogram (){
 if ! [ -x "$(command -v $1)" ]; then
-  echo 'Error: $1 is not installed or in path.' >&2
-  exit 1
+	echo 'Error: $1 is not installed or in path.' >&2
+	exit 1
+else
+	echo "$1 is installed"
 fi
+}
 
 if [ `sudo whoami` != "root" ]; then
 	issudo=true
@@ -27,23 +30,24 @@ fi
 
 
 # Clone an template file if not exists
-copytemplate ()
-{
+copytemplate (){
 if [ ! -f $2 ]; then
   cp $1 $2
 fi
 }
 
+echo "Check for requrements:"
 checkprogram mvn
 checkprogram git
 checkprogram java
+echo ""
 #checkprogram soffice
 
-
-copytemplate cp ${JEVIS_TEMPLATE}/jevis.conf ${JEVIS_HOME}/etc/jevis.conf
-copytemplate cp ${JEVIS_TEMPLATE}/jevis.xml ${JEVIS_HOME}/etc/jevis.xml
-copytemplate cp ${JEVIS_TEMPLATE}/log4j.properties ${JEVIS_HOME}/etc/log4j.properties
-copytemplate cp ${JEVIS_TEMPLATE}/template_log4j2.xml ${JEVIS_HOME}/etc/log4j2.xml
+echo "Generate config files:"
+copytemplate ${JEVIS_TEMPLATE}/jevis.conf ${JEVIS_HOME}/etc/jevis.conf
+copytemplate ${JEVIS_TEMPLATE}/jevis.xml ${JEVIS_HOME}/etc/jevis.xml
+copytemplate ${JEVIS_TEMPLATE}/log4j.properties ${JEVIS_HOME}/etc/log4j.properties
+copytemplate ${JEVIS_TEMPLATE}/template_log4j2.xml ${JEVIS_HOME}/etc/log4j2.xml
 
 cd ${JEVIS_HOME}
 git pull
