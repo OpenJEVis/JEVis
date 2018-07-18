4fabed885b29d6e8d7a442d3ebb2a7055c4f3f9f
Florian Simon <florian.simon@envidatec.com>
2018-07-18 17:21:08 +0200

    #85

---
+++ JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/object/extension/calculation/VariablesBox.java
@@ -0,0 +1,101 @@
+package org.jevis.jeconfig.plugin.object.extension.calculation;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import org.jevis.api.*;
+import org.jevis.application.jevistree.TreeHelper;
+import org.jevis.application.resource.ResourceLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VariablesBox extends FlowPane {
+
+
+    private List<JEVisObject> variables = new ArrayList<>();
+    private FormelBox expression;
+    private JEVisObject calcObject;
+
+    public VariablesBox() {
+        super();
+//        setSpacing(5);
+        setVgap(8);
+        setHgap(4);
+        setPrefWrapLength(200);
+    }
+
+    public void bindVaribaleBox(FormelBox expression, JEVisObject obj){
+        this.calcObject=obj;
+        this.expression=expression;
+        listVariables(calcObject);
+    }
+
+
+
+
+    private void buildVarButton(JEVisObject inpuObject){
+
+        try {
+            Button button = new Button();
+            button.setFocusTraversable(false);
+            JEVisAttribute id = inpuObject.getAttribute("Identifier");
+            if(id!=null) {
+                JEVisSample value = id.getLatestSample();
+                if (value != null && !value.getValueAsString().isEmpty()) {
+                    String name = value.getValueAsString();
+                    button.setText(name);
+
+                    button.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent event) {
+                            expression.addExpression("#{" + name + "}");
+                        }
+                    });
+                } else {
+                    button.setText("*no name* ID: " + inpuObject.getID());
+                    button.disableProperty().setValue(true);
+                }
+
+                getChildren().add(button);
+            }
+
+        } catch (Exception np) {
+            np.printStackTrace();
+        }
+    }
+
+    private void listVariables(JEVisObject obj) {
+        getChildren().clear();
+
+        Button addInputButton = new Button("",ResourceLoader.getImage("list-add.png", 15, 15));
+        addInputButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    TreeHelper.createCalcInput(obj);
+                    listVariables(obj);
+                    expression.updateVariables();
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+        getChildren().add(addInputButton);
+
+        try {
+            JEVisClass input = obj.getDataSource().getJEVisClass("Input");
+            for (JEVisObject var : obj.getChildren()) {
+
+                buildVarButton(var);
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+}
