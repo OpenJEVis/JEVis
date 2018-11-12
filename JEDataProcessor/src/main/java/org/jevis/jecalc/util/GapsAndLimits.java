4521735f1343c5f920ba378055e75991fa18d646
Gerrit Schutz <gerrit.schutz@freenet.de>
2018-11-12 17:41:09 +0100

    Fixed some issues with gaps

--- JEDataProcessor/src/main/java/org/jevis/jecalc/util/GapsAndLimits.java
+++ JEDataProcessor/src/main/java/org/jevis/jecalc/util/GapsAndLimits.java
@@ -335,7 +335,6 @@ public class GapsAndLimits {
                         }
                     }
                 }
-                logger.info("blablablab");
                 break;
         }
     }
