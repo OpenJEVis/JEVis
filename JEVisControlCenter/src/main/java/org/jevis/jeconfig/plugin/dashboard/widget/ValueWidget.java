f8fbdda2fe8c60f983cb6fb3713a74ebc5ec918b
EnvidatecFS <florian.simon@envidatec.com>
2019-09-06 11:07:01 +0200

    Start implementing limit function

--- JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/dashboard/widget/ValueWidget.java
+++ JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/dashboard/widget/ValueWidget.java
@@ -1,6 +1,9 @@
 package org.jevis.jeconfig.plugin.dashboard.widget;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.util.concurrent.AtomicDouble;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -12,6 +15,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jevis.api.JEVisSample;
@@ -40,6 +44,10 @@ public class ValueWidget extends Widget {
     private DataModelDataHandler sampleHandler;
     private StringProperty labelText = new SimpleStringProperty("n.a.");
     private LimitColoring limitColoring;
+    private Limit limit = null;
+
+
+    public static String LIMIT_NODE_NAME = "limit";
 
     public ValueWidget(DashboardControl control, WidgetPojo config) {
         super(control, config);
@@ -90,7 +98,7 @@ public class ValueWidget extends Widget {
 
 
         this.labelText.setValue("n.a.");
-
+        AtomicDouble total = new AtomicDouble(Double.MIN_VALUE);
         try {
             if (!this.sampleHandler.getDataModel().isEmpty()) {
                 ChartDataModel dataModel = this.sampleHandler.getDataModel().get(0);
@@ -102,12 +110,13 @@ public class ValueWidget extends Widget {
 
                 results = dataModel.getSamples();
                 if (!results.isEmpty()) {
-                    Double total = DataModelDataHandler.getTotal(results);
-                    this.labelText.setValue((this.nf.format(total)) + " " + unit);
-                    if (this.limitColoring != null) {
-                        System.out.println("limitColoring formate");
-                        this.limitColoring.formateLabel(this.label, total);
-                    }
+                    total.set(DataModelDataHandler.getTotal(results));
+
+                    this.labelText.setValue((this.nf.format(total.get())) + " " + unit);
+//                    if (this.limitColoring != null) {
+//                        System.out.println("limitColoring formate");
+//                        this.limitColoring.formateLabel(this.label, total.get());
+//                    }
                 }
 
             } else {
@@ -119,8 +128,32 @@ public class ValueWidget extends Widget {
             ex.printStackTrace();
         }
 
+        limit = new Limit();
+        limit.setHasLowerLimit(true);
+        limit.setHasUpperLimit(true);
+        limit.setLowerLimit(80000d);
+        limit.setUpperLimit(150000d);
+
         Platform.runLater(() -> {
             this.label.setText(this.labelText.getValue());
+            Color fontColor = this.config.getFontColor();
+
+            if (limit != null && total.get() != Double.MIN_VALUE) {
+
+                if (limit.hasLowerLimit) {
+                    if (total.get() < limit.getLowerLimit()) {
+                        fontColor = Color.RED;
+                    }
+                }
+                if (limit.hasUpperLimit) {
+                    if (total.get() > limit.getUpperLimit()) {
+                        fontColor = Color.GREEN;
+                    }
+                }
+            }
+            this.label.setTextFill(fontColor);
+
+
             showProgressIndicator(false);
         });
 
@@ -163,6 +196,13 @@ public class ValueWidget extends Widget {
         logger.debug("init");
         this.sampleHandler = new DataModelDataHandler(getDataSource(), this.config.getConfigNode(WidgetConfig.DATA_HANDLER_NODE));
         this.sampleHandler.setMultiSelect(false);
+
+        try {
+            this.limit = new Limit(this.config.getConfigNode(LIMIT_NODE_NAME));
+        } catch (Exception ex) {
+            this.limit = new Limit();
+        }
+
         this.label.setPadding(new Insets(0, 8, 0, 8));
         setGraphic(this.label);
     }
@@ -179,11 +219,108 @@ public class ValueWidget extends Widget {
         ObjectNode dashBoardNode = super.createDefaultNode();
         dashBoardNode
                 .set(JsonNames.Widget.DATA_HANDLER_NODE, this.sampleHandler.toJsonNode());
+
+
+        if (limit != null) {
+            dashBoardNode
+                    .set(LIMIT_NODE_NAME, limit.toJSON());
+        }
+
+
         return dashBoardNode;
     }
 
     @Override
     public ImageView getImagePreview() {
         return JEConfig.getImage("widget/ValueWidget.png", this.previewSize.getHeight(), this.previewSize.getWidth());
+    }
+
+
+    class Limit {
+
+
+        Double lowerLimit = 0d;
+        Double upperLimit = 0d;
+
+        boolean hasLowerLimit = false;
+        boolean hasUpperLimit = false;
+
+        Color upperLimitColor = Color.RED;
+        Color lowerLimitColor = Color.RED;
+
+        public Limit() {
+        }
+
+        public Limit(JsonNode jsonNode) {
+            hasLowerLimit = jsonNode.get("lowerLimit").asBoolean(false);
+            hasUpperLimit = jsonNode.get("upperLimit").asBoolean(false);
+
+            lowerLimit = jsonNode.get("lowerLimitValue").asDouble(0);
+            upperLimit = jsonNode.get("upperLimitValue").asDouble(0);
+
+            lowerLimitColor = Color.valueOf(jsonNode.get("lowerLimitColor").asText(Color.RED.toString()));
+            upperLimitColor = Color.valueOf(jsonNode.get("upperLimitColor").asText(Color.RED.toString()));
+        }
+
+        public ObjectNode toJSON() {
+            ObjectNode dataNode = JsonNodeFactory.instance.objectNode();
+            dataNode.put("lowerLimit", isHasLowerLimit());
+            dataNode.put("upperLimit", isHasUpperLimit());
+            dataNode.put("lowerLimitValue", lowerLimit);
+            dataNode.put("upperLimitValue", upperLimit);
+            dataNode.put("lowerLimitColor", lowerLimitColor.toString());
+            dataNode.put("upperLimitColor", upperLimitColor.toString());
+            return dataNode;
+        }
+
+        public Double getLowerLimit() {
+            return lowerLimit;
+        }
+
+        public void setLowerLimit(Double lowerLimit) {
+            this.lowerLimit = lowerLimit;
+        }
+
+        public Double getUpperLimit() {
+            return upperLimit;
+        }
+
+        public void setUpperLimit(Double upperLimit) {
+            this.upperLimit = upperLimit;
+        }
+
+        public boolean isHasLowerLimit() {
+            return hasLowerLimit;
+        }
+
+        public void setHasLowerLimit(boolean hasLowerLimit) {
+            this.hasLowerLimit = hasLowerLimit;
+        }
+
+        public boolean isHasUpperLimit() {
+            return hasUpperLimit;
+        }
+
+        public void setHasUpperLimit(boolean hasUpperLimit) {
+            this.hasUpperLimit = hasUpperLimit;
+        }
+
+        public Color getUpperLimitColor() {
+            return upperLimitColor;
+        }
+
+        public void setUpperLimitColor(Color upperLimitColor) {
+            this.upperLimitColor = upperLimitColor;
+        }
+
+        public Color getLowerLimitColor() {
+            return lowerLimitColor;
+        }
+
+        public void setLowerLimitColor(Color lowerLimitColor) {
+            this.lowerLimitColor = lowerLimitColor;
+        }
+
+
     }
 }
