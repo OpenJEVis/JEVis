d17761fa6072a1441fe7d7fbcca91fb7542c52aa
Gerrit Schutz <gerrit.schutz@freenet.de>
2019-10-16 13:21:58 +0200

    JECC - Währungseinheiten wie € werden nicht angezeigt [Prio hoch] fixes #1296 JECC - Wenn man die Einheit € benutzt, wird im Baum "Unknown" statt "€" angezeigt fixes #1214

--- JEAPI-WS/src/main/java/org/jevis/jeapi/ws/JEVisDataSourceWS.java
+++ JEAPI-WS/src/main/java/org/jevis/jeapi/ws/JEVisDataSourceWS.java
@@ -158,7 +158,7 @@ public class JEVisDataSourceWS implements JEVisDataSource {
      * @throws JEVisException
      */
     @Override
-    public List<JEVisAttribute> getAttributes() throws JEVisException {
+    public void getAttributes() throws JEVisException {
         logger.debug("Get all attributes Objects");
 
         try {
@@ -194,42 +194,42 @@ public class JEVisDataSourceWS implements JEVisDataSource {
 
                 /**
                  * TODO:
-                 * 1. use fixMissingAttributes(attributes); fi fix missing attribes
-                 * 2. is the function belov oK?
+                 * 1. use fixMissingAttributes(attributes); fi fix missing attributes
+                 * 2. is the function below oK?
                  */
 
                 /**
                  * Give objects which have no attributes an empty list
                  */
-                this.objectCache.keySet().forEach(aLong -> {
-                    if (!this.attributeCache.containsKey(aLong)) {
-                        this.attributeCache.put(aLong, new ArrayList<>());
-                    }
-                    fixMissingAttributes(getObject(aLong), this.attributeCache.get(aLong));
-
-                });
-                logger.debug("done fixing attributes");
+//                this.objectCache.keySet().forEach(aLong -> {
+//                    if (!this.attributeCache.containsKey(aLong)) {
+//                        this.attributeCache.put(aLong, new ArrayList<>());
+//                    }
+//                    fixMissingAttributes(getObject(aLong), this.attributeCache.get(aLong));
+//
+//                });
+//                logger.debug("done fixing attributes");
 
                 this.allAttributesPreloaded = true;
 
-                return attributeList;
+//                return attributeList;
             }
 
             /**
              * Load from cache
              */
-            List<JEVisAttribute> result = new ArrayList<>();
-            this.attributeCache.values().forEach(result::addAll);
+//            List<JEVisAttribute> result = new ArrayList<>();
+//            this.attributeCache.values().forEach(result::addAll);
 //                System.out.println("end get attributes");
-            return result;
+//            return result;
 
         } catch (ProtocolException ex) {
             logger.error(ex);
             //TODO: throw excption?! so the other function can handel it?
-            return new ArrayList<>();
+//            return new ArrayList<>();
         } catch (Exception ex) {
             logger.error(ex);
-            return new ArrayList<>();
+//            return new ArrayList<>();
         }
     }
 
