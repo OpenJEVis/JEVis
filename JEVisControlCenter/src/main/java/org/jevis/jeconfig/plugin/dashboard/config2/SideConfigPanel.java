2bda869ff7ab472c8d8bd7da2f676df1e52c2bc8
Gerrit Schutz <gschutz@freenet.de>
2022-12-02 16:19:53 +0100

    JECC - DashboardPlugin - fixed wrong translation string

--- JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/dashboard/config2/SideConfigPanel.java
+++ JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/dashboard/config2/SideConfigPanel.java
@@ -48,7 +48,7 @@ public class SideConfigPanel extends GridPane {
     private final Spinner<Integer> fontSizeSpinner = new Spinner<Integer>(5, 50, 12);
     private final FontWeightBox fontWeightBox = new FontWeightBox();
     private final FontPostureBox fontPostureBox = new FontPostureBox();
-    private final JFXCheckBox fontUnderlined = new JFXCheckBox(I18n.getInstance().getString("plugin.dashboard.controls.underline"));
+    private final JFXCheckBox fontUnderlined = new JFXCheckBox(I18n.getInstance().getString("plugin.dashboard.controls.fontunderlined"));
     private final Spinner<Integer> precisionSpinner = new Spinner<Integer>(0, 20, 2);
     private final Label fColorLabel = new Label(I18n.getInstance().getString("plugin.dashboard.edit.general.fontcolor"));
     private final Label bgColorLabel = new Label(I18n.getInstance().getString("plugin.dashboard.edit.general.color"));
