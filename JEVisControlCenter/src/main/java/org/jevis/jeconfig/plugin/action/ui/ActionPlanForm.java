4a8fdbb74d062f1767805a60c06d0605ebf9b201
Florian Simon <florian.simon@envidatec.com>
2023-01-08 19:18:24 +0100

    JECC - Action - Add EnPI link gui

--- JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/action/ui/ActionPlanForm.java
+++ JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/action/ui/ActionPlanForm.java
@@ -1,9 +1,15 @@
 package org.jevis.jeconfig.plugin.action.ui;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import org.jevis.api.JEVisClass;
+import org.jevis.api.JEVisObject;
 import org.jevis.commons.i18n.I18n;
 import org.jevis.jeconfig.Icon;
 import org.jevis.jeconfig.JEConfig;
@@ -11,6 +17,8 @@ import org.jevis.jeconfig.TopMenu;
 import org.jevis.jeconfig.plugin.action.data.ActionPlan;
 import org.jevis.jeconfig.tool.ScreenSize;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class ActionPlanForm extends Alert {
@@ -20,13 +28,16 @@ public class ActionPlanForm extends Alert {
     Label statusLabel = new Label("Status");
     Label fieldsLabel = new Label("Bereiche");
     Label mediumLabel = new Label("Medien");
+    Label enpiLabel = new Label("EnPI");
 
     JFXTextField nameField = new JFXTextField();
 
     ListView<String> statusListView = new ListView<>();
     ListView<String> fieldsListView = new ListView<>();
     ListView<String> mediumListView = new ListView<>();
+    ListView<JEVisObject> enpiListView = new ListView<>();
     private ActionPlan actionPlan;
+    StackPane stackPane = new StackPane();
 
     public ActionPlanForm(ActionPlan actionPlan) {
         super(AlertType.INFORMATION);
@@ -46,7 +57,7 @@ public class ActionPlanForm extends Alert {
         GridPane statusPane = buildCustomList(statusListView);
         GridPane mediumPane = buildCustomList(mediumListView);
         GridPane fieldPane = buildCustomList(fieldsListView);
-
+        GridPane enpiPane = buildENPIList(enpiListView);
 
         GridPane gridPane = new GridPane();
         gridPane.setHgap(12);
@@ -59,10 +70,14 @@ public class ActionPlanForm extends Alert {
         gridPane.add(mediumPane, 0, 4, 3, 1);
         gridPane.add(fieldsLabel, 0, 5);
         gridPane.add(fieldPane, 0, 6, 3, 1);
+        gridPane.add(enpiLabel, 0, 7);
+        gridPane.add(enpiPane, 0, 8, 3, 1);
+
 
         gridPane.add(nameField, 1, 0);
 
-        getDialogPane().setContent(gridPane);
+        stackPane.getChildren().add(gridPane);
+        getDialogPane().setContent(stackPane);
         updateView(actionPlan);
     }
 
@@ -75,6 +90,82 @@ public class ActionPlanForm extends Alert {
         statusListView.setItems(actionPlan.getStatustags());
         fieldsListView.setItems(actionPlan.getFieldsTags());
         mediumListView.setItems(actionPlan.getMediumTags());
+    }
+
+    private GridPane buildENPIList(ListView<JEVisObject> listView) {
+        enpiListView.setCellFactory(new Callback<ListView<JEVisObject>, ListCell<JEVisObject>>() {
+            @Override
+            public ListCell<JEVisObject> call(ListView<JEVisObject> param) {
+                return new ListCell<JEVisObject>() {
+                    @Override
+                    protected void updateItem(JEVisObject item, boolean empty) {
+                        super.updateItem(item, empty);
+                        //System.out.println("Update Item: " + item + "   is empty: " + empty);
+                        if (item != null) {
+                            setText(item.getName());
+                            try {
+                                //setTooltip(new Tooltip(objectRelations.getObjectPath(item) + objectRelations.getRelativePath(item) + item.getName()));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            setText(null);
+                        }
+
+                    }
+                };
+            }
+        });
+        //actionPlan.getEnpis().add(actionPlan.getObject());
+        enpiListView.setItems(actionPlan.getEnpis());
+        enpiListView.selectionModelProperty().addListener(new ChangeListener<MultipleSelectionModel<JEVisObject>>() {
+            @Override
+            public void changed(ObservableValue<? extends MultipleSelectionModel<JEVisObject>> observable, MultipleSelectionModel<JEVisObject> oldValue, MultipleSelectionModel<JEVisObject> newValue) {
+
+            }
+        });
+
+        Button addButton = new Button("", JEConfig.getSVGImage(Icon.PLUS_CIRCLE, 8, 14));
+        Button removeButton = new Button("", JEConfig.getSVGImage(Icon.MINUS_CIRCLE, 8, 14));
+        addButton.setOnAction(event -> {
+            try {
+                List<JEVisClass> classes = new ArrayList<>();
+
+                TreeSelectionDialog treeSelectionDialog = new TreeSelectionDialog(actionPlan.getObject().getDataSource(), classes, SelectionMode.MULTIPLE, new ArrayList<>(), false);
+                Optional<ButtonType> optional = treeSelectionDialog.showAndWait();
+                if (optional.get() == treeSelectionDialog.buttonOK) {
+                    System.out.println("apply");
+                    treeSelectionDialog.getUserSelection().forEach(userSelection -> {
+                        actionPlan.getEnpis().add(userSelection.getSelectedObject());
+                    });
+                }
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+
+        });
+
+        removeButton.setOnAction(event -> {
+            try {
+                listView.getItems().remove(listView.getSelectionModel().getSelectedItem());
+            } catch (Exception ex) {
+            }
+        });
+
+        listView.setMaxHeight(100);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(8);
+        gridPane.setVgap(8);
+
+        gridPane.add(listView, 0, 0, 1, 3);
+        gridPane.add(addButton, 1, 0);
+        gridPane.add(removeButton, 1, 1);
+
+        return gridPane;
+
     }
 
     private GridPane buildCustomList(ListView<String> listView) {
