d9f0e2eaa72577cd71b8a96b979aa731438596e3
EnvidatecFS <florian.simon@envidatec.com>
2023-03-13 16:23:21 +0100

    JECC - ActionPlugin - what's wrong with git why are this ignored

---
+++ JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/action/ui/control/CurrencyColumnCell.java
@@ -0,0 +1,37 @@
+package org.jevis.jeconfig.plugin.action.ui.control;
+
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
+import org.jevis.jeconfig.plugin.action.data.ActionData;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+public class CurrencyColumnCell implements Callback<TableColumn<ActionData, Double>, TableCell<ActionData, Double>> {
+
+    NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+
+    {
+        currencyFormat.setCurrency(Currency.getInstance(Locale.GERMANY));
+    }
+
+    ;
+
+    @Override
+    public TableCell<ActionData, Double> call(TableColumn<ActionData, Double> param) {
+        return new TableCell<ActionData, Double>() {
+            @Override
+            protected void updateItem(Double item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (item != null && !empty) {
+                    setText(currencyFormat.format(item));
+                } else {
+                    setText(null);
+                }
+            }
+        };
+    }
+}
---
+++ JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/action/ui/control/StringListColumnCell.java
@@ -0,0 +1,37 @@
+package org.jevis.jeconfig.plugin.action.ui.control;
+
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
+import org.jevis.jeconfig.plugin.action.data.ActionData;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+public class StringListColumnCell implements Callback<TableColumn<ActionData, String>, TableCell<ActionData, String>> {
+
+    NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+
+    {
+        currencyFormat.setCurrency(Currency.getInstance(Locale.GERMANY));
+    }
+
+    ;
+
+    @Override
+    public TableCell<ActionData, String> call(TableColumn<ActionData, String> param) {
+        return new TableCell<ActionData, String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (item != null && !empty) {
+                    setText(item.replace(";", ", "));
+                } else {
+                    setText(null);
+                }
+            }
+        };
+    }
+}
