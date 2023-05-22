39e311dd682c82d52723c1a8c1da0d16089c293a
EnvidatecFS <florian.simon@envidatec.com>
2023-05-22 13:42:40 +0200

    DataCollector - Add SQl Driver

---
+++ DataCollector/SQL-Driver/src/main/java/org/jevis/datacollector/sqldriver/RequestParameters.java
@@ -0,0 +1,146 @@
+package org.jevis.datacollector.sqldriver;
+
+import org.apache.logging.log4j.LogManager;
+import org.jevis.api.JEVisAttribute;
+import org.jevis.api.JEVisObject;
+import org.jevis.api.JEVisSample;
+import org.jevis.commons.classes.JC;
+import org.jevis.commons.driver.VarFiller;
+import org.jevis.commons.object.plugin.TargetHelper;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestParameters {
+
+    private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(RequestParameters.class);
+    private JEVisAttribute targetAtt;
+    private String query;
+    private String exeQuery;
+
+    private String valueColumn;
+    private String timeStampColumn;
+    private String timeStampFormate;
+    private String noteColumn;
+    private String valueFormate;
+    private JEVisAttribute lastReadoutAttribute;
+    private JEVisAttribute statusAttribute;
+    private DateTime lastLog = new DateTime();
+
+    public RequestParameters(JEVisObject queryObject) throws Exception {
+
+        query = Parameters.getAttValue(queryObject, "Query", "", false);
+        JEVisAttribute target = queryObject.getAttribute("Target");
+        TargetHelper th = new TargetHelper(queryObject.getDataSource(), target);
+        if (!th.isValid()) throw new RuntimeException("target not reachable");
+        targetAtt = th.getAttribute().get(0);
+        JEVisSample lastSample = targetAtt.getLatestSample();
+
+        lastReadoutAttribute = queryObject.getAttribute(JC.Channel.a_LastReadout);
+        statusAttribute = queryObject.getAttribute("Status Log");
+
+        timeStampColumn = Parameters.getAttValue(queryObject, "Timestamp Column", "", false);
+        valueColumn = Parameters.getAttValue(queryObject, "Value Column", "", false);
+        timeStampFormate = Parameters.getAttValue(queryObject, "Timestamp Formate", "TIMESTAMP", true);
+        noteColumn = Parameters.getAttValue(queryObject, "Note Column", "", true);
+        valueFormate = Parameters.getAttValue(queryObject, "Value Formate", "DOUBLE", true);
+
+        Map<VarFiller.Variable, VarFiller.VarFunction> variables = new HashMap<>();
+
+        DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        if (lastSample != null) {
+            variables.put(VarFiller.Variable.LAST_TS, () -> {
+                try {
+                    return fmt.print(lastSample.getTimestamp());
+                } catch (Exception e) {
+                    return "";
+                }
+            });
+        } else {
+            variables.put(VarFiller.Variable.LAST_TS, () -> {
+                try {
+                    return fmt.print(new DateTime(1980, 01, 01, 01, 01));
+                } catch (Exception e) {
+                    return "";
+                }
+            });
+        }
+
+
+        VarFiller varFiller = new VarFiller(query, variables);
+        exeQuery = varFiller.getFilledURIString();
+
+    }
+
+    public JEVisAttribute getTarget() {
+        return targetAtt;
+    }
+
+    public String getExecutableQuery() {
+        return exeQuery;
+    }
+
+    public String valueColumn() {
+        return valueColumn;
+    }
+
+    public String timeStampColumn() {
+        return timeStampColumn;
+    }
+
+    public String timeStampFormate() {
+        return timeStampFormate;
+    }
+
+    public String noteColumn() {
+        return noteColumn;
+    }
+
+    public String valueFormate() {
+        return valueFormate;
+    }
+
+    public void updateLastReadout() {
+        try {
+            JEVisSample sample = lastReadoutAttribute.buildSample(new DateTime(), targetAtt.getLatestSample().getTimestamp());
+            sample.commit();
+        } catch (Exception ex) {
+            logger.error("Cant log status to JEVis", ex, ex);
+        }
+    }
+
+    /**
+     * Log a status message into JEVis.
+     * <p>
+     * We can only have one msg per second, so we check for the last msg
+     *
+     * @param status
+     * @param ts
+     * @param msg
+     */
+    public void logStatus(int status, DateTime ts, String msg) {
+        try {
+            logger.error("Status: {} msg: {}", status, msg);
+            JEVisSample sample = null;
+            if (lastLog.plusSeconds(1).isAfter(ts)) {
+                sample = statusAttribute.buildSample(ts, status + ": " + msg);
+            } else {
+                sample = statusAttribute.buildSample(lastLog.plusSeconds(1), status + ": " + msg);
+            }
+            sample.commit();
+        } catch (Exception ex) {
+            logger.error("Cant log status to JEVis", ex, ex);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "RequestParameters{" +
+                "targetAtt=" + targetAtt +
+                ", exeQuery='" + exeQuery + '\'' +
+                '}';
+    }
+}
