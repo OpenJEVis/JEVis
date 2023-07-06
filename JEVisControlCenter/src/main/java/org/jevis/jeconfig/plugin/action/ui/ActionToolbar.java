e8b26eacd92ee1ce20bab3bc994578a49130799a
EnvidatecFS <florian.simon@envidatec.com>
2023-07-06 14:59:05 +0200

    JECC - Action - Removed delete plan button fom toolbar JECC - Action - Changed toolbar icons

--- JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/action/ui/ActionToolbar.java
+++ JEVisControlCenter/src/main/java/org/jevis/jeconfig/plugin/action/ui/ActionToolbar.java
@@ -22,9 +22,9 @@ public class ActionToolbar extends ToolBar {
     private final double iconSize = 20;
     private final ToggleButton actionPlanConfig = new ToggleButton("", JEConfig.getSVGImage(Icon.SETTINGS, iconSize, iconSize));
     private final ToggleButton openForm = new ToggleButton("", JEConfig.getSVGImage(Icon.PREVIEW, iconSize, iconSize));
-    private final ToggleButton newAction = new ToggleButton("", JEConfig.getSVGImage(Icon.PLAYLIST_ADD, iconSize, iconSize));
+    private final ToggleButton newAction = new ToggleButton("", JEConfig.getSVGImage(Icon.PLUS, iconSize, iconSize));
     private final ToggleButton newPlan = new ToggleButton("", JEConfig.getSVGImage(Icon.FOLDER_OPEN, iconSize, iconSize));
-    private final ToggleButton deleteAction = new ToggleButton("", JEConfig.getSVGImage(Icon.PLAYLIST_REMOVE, iconSize, iconSize));
+    private final ToggleButton deleteAction = new ToggleButton("", JEConfig.getSVGImage(Icon.DELETE, iconSize, iconSize));
     private final ToggleButton deletePlan = new ToggleButton("", JEConfig.getSVGImage(Icon.DELETE, iconSize, iconSize));
     private final ToggleButton reloadButton = new ToggleButton("", JEConfig.getSVGImage(Icon.REFRESH, this.iconSize, this.iconSize));
     private final ToggleButton exportPDF = new ToggleButton("", JEConfig.getSVGImage(Icon.PDF, this.iconSize, this.iconSize));
@@ -39,7 +39,7 @@ public class ActionToolbar extends ToolBar {
 
         Separator sep1 = new Separator();
         Separator sep2 = new Separator();
-        getItems().setAll(newPlan, actionPlanConfig, deletePlan, reloadButton,
+        getItems().setAll(newPlan, actionPlanConfig, reloadButton,
                 sep1, newAction, deleteAction, openForm,
                 sep2, exportPDF,
                 JEVisHelp.getInstance().buildSpacerNode(), helpButton, infoButton);
