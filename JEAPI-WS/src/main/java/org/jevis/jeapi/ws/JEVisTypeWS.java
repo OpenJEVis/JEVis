752accc009cafc9ab5095e26602de212f0c2d5ca
dak <daniel.klinker91@icloud.com>
2023-09-22 13:23:19 +0200

    JECC - meters v2 - rollback JEAPI-WS

--- JEAPI-WS/src/main/java/org/jevis/jeapi/ws/JEVisTypeWS.java
+++ JEAPI-WS/src/main/java/org/jevis/jeapi/ws/JEVisTypeWS.java
@@ -199,18 +199,13 @@ public class JEVisTypeWS implements JEVisType {
     }
 
     @Override
-    public int hashCode() {
-        return getName().hashCode();
-    }
-
-    @Override
     public boolean equals(Object obj
     ) {
         try {
             if (obj == null) {
                 return false;
             }
-            if (getClass() != obj.getClass() && !getClass().isAssignableFrom(obj.getClass())) {
+            if (getClass() != obj.getClass()) {
                 return false;
             }
             /**
@@ -220,7 +215,7 @@ public class JEVisTypeWS implements JEVisType {
             if ((this.getName() == null) ? (other.getName() != null) : !this.getName().equals(other.getName())) {
                 return false;
             }
-            return this.getJEVisClass() == other.getJEVisClass() || this.getClass().isAssignableFrom(other.getClass()) ||other.getClass().isAssignableFrom(this.getClass()) || (this.getJEVisClass() != null && this.getJEVisClass().equals(other.getJEVisClass()));
+            return this.getJEVisClass() == other.getJEVisClass() || (this.getJEVisClass() != null && this.getJEVisClass().equals(other.getJEVisClass()));
         } catch (Exception ex) {
             return false;
         }
