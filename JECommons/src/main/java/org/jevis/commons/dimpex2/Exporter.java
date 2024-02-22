697d8b4da3904ecb5d3a342dc7ed4e5462f13d56
EnvidatecFS <florian.simon@envidatec.com>
2024-02-22 14:48:25 +0100

    JECC - Commons - DIMPEX version 2 work in process

--- JECommons/src/main/java/org/jevis/commons/dimpex2/Exporter.java
+++ JECommons/src/main/java/org/jevis/commons/dimpex2/Exporter.java
@@ -3,28 +3,25 @@ package org.jevis.commons.dimpex2;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.commons.math3.analysis.function.Exp;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jevis.api.JEVisEvent;
 import org.jevis.api.JEVisEventListener;
 import org.jevis.api.JEVisException;
 import org.jevis.api.JEVisObject;
 
 import javax.swing.event.EventListenerList;
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.EventListener;
 import java.util.List;
 import java.util.UUID;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Exporter {
 
-    private final EventListenerList listeners = new EventListenerList();
     private static final Logger logger = LogManager.getLogger(Exporter.class);
-
+    private final EventListenerList listeners = new EventListenerList();
     //Take JEVis Object
     //Generate pojo
     //get children list, if selected
@@ -38,12 +35,12 @@ public class Exporter {
 
         List<ObjectNode> nodes = new ArrayList<>();
 
-        jeVisObject.getChildren()
+        //jeVisObject.getChildren()
         nodes.forEach(jsonNodes -> {
-            notifyListeners(new DimpExEvent(this, DimpExEvent.TYPE.OBJECT_TMPFILE_CREATE,jsonNodes,"Create TMP file: "+jsonNodes.asText()));
-            File file = new File(tmpDir.getAbsolutePath()+File.separator+);
+            notifyListeners(new DimpExEvent(this, DimpExEvent.TYPE.OBJECT_TMPFILE_CREATE, jsonNodes, "Create TMP file: " + jsonNodes.asText()));
+            File file = new File(tmpDir.getAbsolutePath() + File.separator);
             objectMapper.writer(new DefaultPrettyPrinter());
-            objectMapper.writeValue(new File(tmpDir.getAbsolutePath()+"D:/cp/dataTwo.json"), jsonDataObject);
+            //objectMapper.writeValue(new File(tmpDir.getAbsolutePath()+"D:/cp/dataTwo.json"), jsonDataObject);
         });
 
 
@@ -53,10 +50,23 @@ public class Exporter {
         }
     }
 
+    private static void walkTree(JEVisObject jeVisObject, List<ExportSetting> fileList) throws JEVisException {
+
+        jeVisObject.getChildren().forEach(child -> {
+            try {
+                ExportSetting setting = new ExportSetting();
+                fileList.add(setting);
+                walkTree(child, fileList);
+            } catch (Exception ex) {
+                logger.error(ex, ex);
+            }
+        });
+    }
+
     private void exportJEVIsPath(File zipFile, JEVisObject rootObj) throws IOException {
 
         File systemTmpDir = new File(System.getProperty("java.io.tmpdir"));
-        File zipTmpDir = new File(System.getProperty("java.io.tmpdir")+File.pathSeparator+UUID.randomUUID());
+        File zipTmpDir = new File(System.getProperty("java.io.tmpdir") + File.pathSeparator + UUID.randomUUID());
         zipTmpDir.mkdirs();
         zipTmpDir.deleteOnExit();
 
@@ -65,27 +75,24 @@ public class Exporter {
 
         try {
             walkTree(rootObj, exportSettingList);
-        }catch (Exception ex){
-            logger.error(ex,ex);
+        } catch (Exception ex) {
+            logger.error(ex, ex);
         }
-
-
 
 
         try (ZipOutputStream zipStream = new ZipOutputStream(new FileOutputStream(zipFile))) {
 
             exportSettingList.forEach(exportSetting -> {
-                FileInputStream inputStream = new FileInputStream(zipTmpDir+);
-                ZipEntry zipEintrag = new ZipEntry(datei.getName());
-                zipStream.putNextEntry(zipEintrag);
+                // FileInputStream inputStream = new FileInputStream(zipTmpDir+);
+                // ZipEntry zipEintrag = new ZipEntry(datei.getName());
+                // zipStream.putNextEntry(zipEintrag);
 
                 byte[] buffer = new byte[1024];
                 int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    zipStream.write(buffer, 0, bytesRead);
-                }
+                //while ((bytesRead = inputStream.read(buffer)) != -1) {
+                //    zipStream.write(buffer, 0, bytesRead);
+                //}
             });
-
 
 
         }
@@ -95,29 +102,16 @@ public class Exporter {
 
     }
 
-    private static void walkTree(JEVisObject jeVisObject, List<ExportSetting> fileList) throws JEVisException {
-
-        jeVisObject.getChildren().forEach(child -> {
-            try {
-                ExportSetting setting = new ExportSetting();
-                fileList.add(setting);
-                walkTree(child, fileList);
-            }catch (Exception ex){
-                logger.error(ex,ex);
-            }
-        });
-    }
-
-    private void createTmpFiles(File tmpDir, List<ExportSetting> objects){
-        if(!tmpDir.exists()) tmpDir.mkdirs();
+    private void createTmpFiles(File tmpDir, List<ExportSetting> objects) {
+        if (!tmpDir.exists()) tmpDir.mkdirs();
 
         objects.forEach(exportSetting -> {
             try {
                 ObjectNode objectNode = objectToNode(exportSetting.getObject());
                 File file = new File(tmpDir + File.separator + exportSetting.getFilename());
                 objectMapper.writeValue(file, objectNode);
-            }catch (Exception ex){
-                logger.error(ex,ex);
+            } catch (Exception ex) {
+                logger.error(ex, ex);
             }
         });
     }
@@ -156,7 +150,7 @@ public class Exporter {
     }
 
     public synchronized void notifyListeners(DimpExEvent event) {
-        logger.trace("Object event[{}] listeners: {} event:",  this.listeners.getListenerCount(), event.getType());
+        logger.trace("Object event[{}] listeners: {} event:", this.listeners.getListenerCount(), event.getType());
         for (DimpExEventListener l : this.listeners.getListeners(DimpExEventListener.class)) {
             l.fireEvent(event);
         }
