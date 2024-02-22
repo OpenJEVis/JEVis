2ff66b57f375c99bc97da265220ab53dcc53f395
EnvidatecFS <florian.simon@envidatec.com>
2024-02-22 14:47:37 +0100

    JECC - Commons - add "UNIX" as possible import datetime formate.

--- JECommons/src/main/java/org/jevis/commons/driver/TimeConverter.java
+++ JECommons/src/main/java/org/jevis/commons/driver/TimeConverter.java
@@ -1,19 +1,19 @@
 /**
  * Copyright (C) 2015 Envidatec GmbH <info@envidatec.com>
- *
+ * <p>
  * This file is part of JECommons.
- *
+ * <p>
  * JECommons is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation in version 3.
- *
+ * <p>
  * JECommons is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License along with
  * JECommons. If not, see <http://www.gnu.org/licenses/>.
- *
+ * <p>
  * JECommons is part of the OpenJEVis project, further project information are
  * published at <http://www.OpenJEVis.org/>.
  */
@@ -25,7 +25,6 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 /**
- *
  * @author broder
  */
 public class TimeConverter {
@@ -39,7 +38,14 @@ public class TimeConverter {
     }
 
     public static DateTime parseDateTime(String input, String pattern, DateTimeZone dateTimeZone) {
-        DateTimeFormatter fmt = DateTimeFormat.forPattern(pattern).withZone(dateTimeZone);
-        return fmt.parseDateTime(input);
+        if (pattern.equals("UNIX")) {
+            Long longDate = Long.parseLong(input) * 1000L;
+            return new DateTime(longDate);
+        } else {
+            DateTimeFormatter fmt = DateTimeFormat.forPattern(pattern).withZone(dateTimeZone);
+            return fmt.parseDateTime(input);
+        }
+
+
     }
 }
