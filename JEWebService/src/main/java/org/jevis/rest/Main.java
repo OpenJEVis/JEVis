b6897bdb725e1b078b2e950c6387ab8806399beb
Gerrit Schutz <gschutz@freenet.de>
2024-07-22 14:46:44 +0200

    JEWebservice - added delete object files clean up (second argument)

--- JEWebService/src/main/java/org/jevis/rest/Main.java
+++ JEWebService/src/main/java/org/jevis/rest/Main.java
@@ -1,5 +1,6 @@
 package org.jevis.rest;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.glassfish.grizzly.http.CompressionConfig;
@@ -16,6 +17,7 @@ import org.jevis.api.JEVisException;
 import org.jevis.commons.utils.JEVisDates;
 import org.jevis.commons.ws.sql.Config;
 import org.jevis.commons.ws.sql.ConnectionFactory;
+import org.jevis.commons.ws.sql.SQLDataSource;
 import org.joda.time.DateTime;
 
 import javax.security.sasl.AuthenticationException;
@@ -30,13 +32,18 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Main class.
  */
 public class Main {
-    public static String VERSION = "JEWebService Version";
     private static final Logger logger = LogManager.getLogger(Main.class);
+    public static String VERSION = "JEWebService Version";
 
     /**
      * Main method.
@@ -48,6 +55,7 @@ public class Main {
         File configfile;
         String newestVerion = "";
         String jarCreationDate = "";
+        boolean cleanFiles = false;
 
         try {
             Path jarPath = Paths.get(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
@@ -83,6 +91,10 @@ public class Main {
             }
         }
         Config.readConfigurationFile(configfile);
+
+        if (args.length >= 2) {
+            cleanFiles = Boolean.parseBoolean(args[1]);
+        }
 
         //Test Connection parameter
 //        for (String para : args) {
@@ -150,6 +162,10 @@ public class Main {
                 new GrizzlyServerShutdownHookThread(server)
         );
 
+        if (cleanFiles) {
+            cleanFilesFolder(dbConn);
+        }
+
         // run
         try {
             server.start();
@@ -164,6 +180,48 @@ public class Main {
         } catch (Exception e) {
             logger.error("Server error: ", e);
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void cleanFilesFolder(Connection dbConn) {
+        try {
+            String dir = Config.getFileDir().getAbsolutePath();
+
+            Set<String> folderContent = listFilesUsingFilesList(dir);
+            List<String> deleteList = new ArrayList<>();
+
+            SQLDataSource ds = new SQLDataSource(dbConn);
+            for (String s : folderContent) {
+                try {
+                    long l = Long.parseLong(s);
+                    if (ds.getObject(l) == null) {
+                        deleteList.add(s);
+                    }
+                } catch (Exception e) {
+                    logger.error("Folder name not a number: {}", s, e);
+                }
+            }
+
+            logger.info("{} file folders without corresponding JEVis objects. Deleting content.", deleteList.size());
+
+            for (String s : deleteList) {
+                File folderToDelete = new File(dir + "/" + s);
+                logger.info("Deleting folder with content: {}", folderToDelete);
+                FileUtils.deleteDirectory(folderToDelete);
+            }
+
+        } catch (Exception e) {
+            logger.error(e);
+        }
+    }
+
+    private static Set<String> listFilesUsingFilesList(String dir) throws IOException {
+        try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+            return stream
+                    .filter(Files::isDirectory)
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .collect(Collectors.toSet());
         }
     }
 }
