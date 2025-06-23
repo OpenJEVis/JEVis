eb77dfb8cc4ef17d5c5ecfd57c24b4f789ac9f91
Gerrit Schutz <gerrit.schutz@envidatec.com>
2025-06-23 11:43:26 +0200

    DataCollector - fixes

--- DataCollector/DWDDriver/src/main/java/org/jevis/dwddatasource/DWDDataSource.java
+++ DataCollector/DWDDriver/src/main/java/org/jevis/dwddatasource/DWDDataSource.java
@@ -132,6 +132,8 @@ public class DWDDataSource implements DataSource {
             for (int i = idString.length(); i < 5; i++) {
                 idString.insert(0, "0");
             }
+            logger.debug("Loading data for station {} with id {}", selectedStation.getName(), idString);
+
             FTPFileFilter filter = ftpFile -> (ftpFile.isFile() && ftpFile.getName().contains(idString.toString()));
 
             stationData.setName(selectedStation.getName());
@@ -155,6 +157,7 @@ public class DWDDataSource implements DataSource {
                                 continue;
                             }
                         }
+                        logger.debug("Found valid file {}", ftpFile.getName());
 
                         ByteArrayOutputStream out = new ByteArrayOutputStream();
                         logger.info("FTPQuery {}", ftpFile.getName());
@@ -168,6 +171,7 @@ public class DWDDataSource implements DataSource {
                         while ((entry = zipInputStream.getNextEntry()) != null) {
                             try {
                                 if (entry.getName().contains("produkt")) {
+                                    logger.debug("Trying to parse file {} in zip", entry.getName());
                                     BufferedReader br = new BufferedReader(new InputStreamReader(zipInputStream), 1024);
                                     List<String> header = new ArrayList<>();
                                     String l;
